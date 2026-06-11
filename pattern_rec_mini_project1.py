d6c1c6f8d0fdc5e2d6b3f4b2122f84b65d94fd30
Edwuina A <edwuinaa@Edwuinas-MacBook-Pro.local>
2026-06-10 21:52:43 -0400

    pushing all files to github

---
+++ pattern_rec_mini_project1.py
@@ -0,0 +1,10 @@
+import wfdb
+import numpy as np
+import matplotlib.pyplot as plt
+record = wfdb.rdrecord('100')
+print(record.p_signal.shape)
+plt.plot(record.p_signal[: 1000 , 0])
+plt.title('EGG Signal')
+plt.xlabel('Sample')
+plt.ylabel('Amplitude')
+plt.show()
